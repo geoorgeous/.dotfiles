a8c2a567ea8db27c9f367810425e8f4f32efa7a3
geoorgeous <gh@georgemcdonagh.dev>
2025-12-29 17:11:52 +0000

    Added script to setup symlinks

---
+++ mksymlinks.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+ln -sf $SCRIPTPATH/.zshrc $HOME/
+ln -sf $SCRIPTPATH/nvim $HOME/.config/
+ln -sf $SCRIPTPATH/kitty $HOME/.config/
+ln -sf $SCRIPTPATH/dconf $HOME/.config/
