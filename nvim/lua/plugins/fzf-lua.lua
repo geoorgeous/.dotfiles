053c888c2b0d41a7d7b722e8c9789aa89b2c5ee5
geoorgeous <gh@georgemcdonagh.dev>
2026-01-11 14:01:29 +0000

    [neovim][fzf-lua] increased preview win size

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,6 +1,13 @@
 return  {
 	"ibhagwan/fzf-lua",
 	opts = {
+		winopts = {
+			height = 0.85,
+			width = 0.85,
+			preview = {
+				horizontal = "right:75%"
+			}
+		},
 		previewers = {
 			git_diff = {
 				cmd_modified = "git diff --color --ignore-space-at-eol HEAD"
