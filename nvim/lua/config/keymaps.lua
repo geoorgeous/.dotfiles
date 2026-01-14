6bf130e13c1f758800ed4dd89a878e2e6e4c717a
geoorgeous <gh@georgemcdonagh.dev>
2026-01-14 12:57:22 +0000

    [nvim] formatting

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,10 +4,10 @@ vim.g.mapleader = ";"
 vim.keymap.set({"", "!"}, "<leader>o", ":LspClangdSwitchSourceHeader<CR>", { desc = "Switch between source and header file"})
 
 -- fzf-lua
-vim.keymap.set({"", "!"}, "<leader>f", ":FzfLua lgrep_curbuf<CR>", { desc = "String search in current buffer"})
+vim.keymap.set({"", "!"}, "<leader>f",     ":FzfLua lgrep_curbuf<CR>", { desc = "String search in current buffer"})
 vim.keymap.set({"", "!"}, "<leader><c-f>", ":FzfLua live_grep<CR>", { desc = "String search in project files"})
-vim.keymap.set({"", "!"}, "<leader>p", ":FzfLua files<CR>", { desc = "List project file"})
+vim.keymap.set({"", "!"}, "<leader>p",     ":FzfLua files<CR>", { desc = "List project file"})
 vim.keymap.set({"", "!"}, "<leader><c-p>", ":FzfLua commands<CR>", { desc = "List Nvim commands"})
-vim.keymap.set({"", "!"}, "<leader>g", ":FzfLua git_status<CR>", { desc = "View git status and preview file diffs"})
-vim.keymap.set({"", "!"}, "<leader>d", ":FzfLua diagnostics_document<CR>", { desc = "String search in current buffer"})
-vim.keymap.set({"", "!"}, "<leader>D", ":FzfLua diagnostics_workspace<CR>", { desc = "String search in current buffer"})
+vim.keymap.set({"", "!"}, "<leader>g",     ":FzfLua git_status<CR>", { desc = "View git status and preview file diffs"})
+vim.keymap.set({"", "!"}, "<leader>d",     ":FzfLua diagnostics_document<CR>", { desc = "String search in current buffer"})
+vim.keymap.set({"", "!"}, "<leader>D",     ":FzfLua diagnostics_workspace<CR>", { desc = "String search in current buffer"})
