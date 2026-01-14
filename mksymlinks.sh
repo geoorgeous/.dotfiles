2c9f812b01a9a11b5b01477e276139135dc6468e
geoorgeous <gh@georgemcdonagh.dev>
2026-01-14 12:49:44 +0000

    added btop symlink

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -5,6 +5,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 ln -sf $SCRIPTPATH/.gitconfig $HOME/
 ln -sf $SCRIPTPATH/.zshrc $HOME/
+ln -sf $SCRIPTPATH/btop $HOME/.config/
 ln -sf $SCRIPTPATH/nvim $HOME/.config/
 ln -sf $SCRIPTPATH/kitty $HOME/.config/
 ln -sf $SCRIPTPATH/dconf $HOME/.config/
